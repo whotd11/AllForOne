f2ae0adaa679c848feda83e3561a94d34a9e3538
PMAg <38464322+whotd11@users.noreply.github.com>
2024-05-09 19:51:52 +0630

    Interceptor added

--- src/main/java/com/pmag/allforone/config/Interceptor.java
+++ src/main/java/com/pmag/allforone/config/Interceptor.java
@@ -1,6 +1,9 @@
 package com.pmag.allforone.config;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
 public class Interceptor implements HandlerInterceptor {
--- src/main/java/com/pmag/allforone/config/WebConfig.java
+++ src/main/java/com/pmag/allforone/config/WebConfig.java
@@ -1,7 +1,15 @@
 package com.pmag.allforone.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurerAdapter  {
+public class WebConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new Interceptor());
+    }
 }
