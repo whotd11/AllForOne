392aecea57b0da356d8330cec563c8a124b39482
PMAg <38464322+whotd11@users.noreply.github.com>
2024-05-09 19:22:05 +0630

    Initial commit

---
+++ src/main/java/com/pmag/allforone/AllForOneApplication.java
@@ -0,0 +1,13 @@
+package com.pmag.allforone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllForOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllForOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pmag/allforone/AllForOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmag.allforone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllForOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
